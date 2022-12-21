1fa376ef45926f7169d599b7b020a4990aa666a0
Mark Crossfield <Mark.Crossfield@autotrader.co.uk>
2022-12-21 18:42:48 +0000

    Day one part two

--- 2022/kotlin/src/main/java/aoc/DayOne.kt
+++ 2022/kotlin/src/main/java/aoc/DayOne.kt
@@ -3,18 +3,27 @@ package aoc
 import java.io.File
 import java.net.URL
 
-fun main(args: Array<String>) {
-    println(DayOne().partOne())
+fun main() {
+    println("Part one: ${DayOne().partOne()}")
+    println("Part two: ${DayOne().partTwo()}")
 }
 
 class DayOne {
     fun partOne(): Any {
+        return eachElvesCalories().maxOf { it }
+    }
+    
+    fun partTwo(): Any {
+        return eachElvesCalories().sortedDescending().slice(0..2).sum()
+    }
+
+    private fun eachElvesCalories(): List<Int> {
         val resource: URL = javaClass.classLoader.getResource("1.txt") ?: throw RuntimeException("Input not found,.")
         val elves: List<String> = File(resource.toURI()).readText().split("\n\n")
         val eachElfsCalories = elves.map { elf ->
             val snacks = elf.split("\n")
-            snacks.stream().mapToInt(Integer::parseInt).reduce(0, Integer::sum);
+            snacks.stream().mapToInt(Integer::parseInt).reduce(0, Integer::sum)
         }
-        return eachElfsCalories.maxOf { it }
+        return eachElfsCalories
     }
 }
